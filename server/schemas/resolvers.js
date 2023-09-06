1020d1d7eaa22086f326fd41f1a3e4abc573c6df
jrdnwlkr <walker.jordan.m@gmail.com>
2023-09-06 16:13:35 -0500

    resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,20 +5,27 @@ const resolvers = {
         me: async (parent, args, context) => {
             return User.findOne({ _id: context.user._id });
         },
-
+        getSingleUser: async (parent, { user = null, params }) => {
+        },
     },
 
     Mutation: {
         login: async (parent, { email, password }) => {
         },
-        addUser: async (parent, args) => {
+        createUser: async (parent, args) => {
+            return User.create(args);
         },
         saveBook: async (parent, args, context) => {
+            return User.findOneAndUpdate(
+                { _id: context.user._id },
+                { $addToSet: { savedBooks: args } },
+                { new: true, runValidators: true }
+            );
         },
-        removeBook: async (parent, args, context) => {
+        deleteBook: async (parent, args, context) => {
+            return User.findOneAndDelete({ _id: context.user._id });
         }
     },
-    User: {
-    
-    }
 };
+
+module.exports = resolvers;
