c05bcb56e2494f447cf934dd9e9d3e9209f4971c
jrdnwlkr <walker.jordan.m@gmail.com>
2023-09-16 19:37:47 -0500

    added Graphql and the SAVE_BOOKS mutation

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -8,8 +8,11 @@ import {
   Row
 } from 'react-bootstrap';
 
+import { useQuery, useMutation } from '@apollo/client';
+import { GET_SEARCHED_BOOKS, SAVE_BOOK } from '../utils/mutations';
+
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
 const SearchBooks = () => {
@@ -17,6 +20,16 @@ const SearchBooks = () => {
   const [searchedBooks, setSearchedBooks] = useState([]);
   // create state for holding our search field data
   const [searchInput, setSearchInput] = useState('');
+
+  const { loading, error, data } = useQuery(GET_SEARCHED_BOOKS, {
+    variables: { searchInput },
+  });
+
+  const [saveBook] = useMutation(SAVE_BOOK);
+
+  
+
+ 
 
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
@@ -36,21 +49,7 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await searchGoogleBooks(searchInput);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { items } = await response.json();
-
-      const bookData = items.map((book) => ({
-        bookId: book.id,
-        authors: book.volumeInfo.authors || ['No author to display'],
-        title: book.volumeInfo.title,
-        description: book.volumeInfo.description,
-        image: book.volumeInfo.imageLinks?.thumbnail || '',
-      }));
+      const bookData = data?.searchedBooks || [];
 
       setSearchedBooks(bookData);
       setSearchInput('');
@@ -61,10 +60,8 @@ const SearchBooks = () => {
 
   // create function to handle saving a book to our database
   const handleSaveBook = async (bookId) => {
-    // find the book in `searchedBooks` state by the matching id
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
 
-    // get token
     const token = Auth.loggedIn() ? Auth.getToken() : null;
 
     if (!token) {
@@ -72,14 +69,10 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
+      const response = await saveBook({
+        variables: { bookData: bookToSave },
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      // if book successfully saves to user's account, save book id to state
-      setSavedBookIds([...savedBookIds, bookToSave.bookId]);
     } catch (err) {
       console.error(err);
     }
@@ -121,8 +114,8 @@ const SearchBooks = () => {
         <Row>
           {searchedBooks.map((book) => {
             return (
-              <Col md="4">
-                <Card key={book.bookId} border='dark'>
+              <Col md="4" key={book.bookId}> 
+              <Card border='dark'>
                   {book.image ? (
                     <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' />
                   ) : null}
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -44,3 +44,38 @@ mutation Mutation($email: String!, $password: String!) {
 }
 
 `;
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($authors: [String], $description: String, $title: String, $bookId: String, $image: String, $link: String) {
+        saveBook(authors: $authors, description: $description, title: $title, bookId: $bookId, image: $image, link: $link){
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+`;
+
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: String) {
+        removeBook(bookId: $bookId){
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }`;
